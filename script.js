c982d9acf47c91ba038377bd623d265f3b7e2a8d
Zachary <ramsumairzachary@gmail.com>
2023-02-27 10:38:57 -0400

    game works up till score is 4-4

--- script.js
+++ script.js
@@ -7,23 +7,31 @@ let conditions = [
 let rockButton = document.getElementById("rock");
 let paperButton = document.getElementById("paper");
 let scissorsButton = document.getElementById("scissors");
-let results = document.getElementById("results");
 
-let firstAni = document.querySelector(".first-ani")
-let secondAni = document.querySelector(".second-ani")
+let firstAni = document.querySelector(".first-ani");
+let secondAni = document.querySelector(".second-ani");
 
+let enemyWeaponImage = document.querySelector(".enemyWeaponImage");
+let roundNumber = document.querySelector(".roundNumber");
+let score = document.querySelector(".score");
+let message = document.querySelector(".message");
 
+setTimeout(function () {
+	firstAni.style.display = "none";
+}, 7000);
+setTimeout(function () {
+	secondAni.style.display = "none";
+}, 13010);
 
-setTimeout(function(){
-    firstAni.style.display = 'none';
-}, 7000); 
-setTimeout(function(){
-    secondAni.style.display = 'none';
-}, 13010); 
-
+let round = 0;
 let playerWins = 0;
 let computerWins = 0;
-results.textContent = `You : ${playerWins} VS CPU : ${computerWins}`;
+
+roundNumber.textContent = `Rounds Played: ${round}`;
+score.textContent = `Your Lives: ${5 - computerWins} ︱ Enemy's Lives: ${
+	5 - playerWins
+}`;
+message.textContent = `Combat Area: Empty`;
 
 let getComputerChoice = () => {
 	let random = Math.floor(Math.random() * (2 - 0 + 1) + 0);
@@ -47,39 +55,109 @@ let playRound = (playerSelection, computerSelection) => {
 		}
 	}
 
+	round++;
+
 	if (computerSelection == playerInfo.ties) {
 		console.log("Game Tied");
-		updateResults();
+		updateResults("Round Tied.", computerSelection);
 		return "Round Tied";
 	}
 
 	if (computerSelection == playerInfo.loses) {
 		console.log(`You Lose! ${playerInfo.loses} beats ${playerInfo.play}`);
 		computerWins++;
-		updateResults();
+		updateResults(
+			`You Lose this round! ${playerInfo.loses} beats ${playerInfo.play}.`,
+			computerSelection
+		);
 		return `You Lose this round! ${playerInfo.loses} beats ${playerInfo.play}`;
 	}
 
 	if (computerSelection == playerInfo.beats) {
 		console.log(`You Win! ${playerInfo.play} beats ${playerInfo.beats}`);
 		playerWins++;
-		updateResults();
+		updateResults(
+			`You Win this round! ${playerInfo.play} beats ${playerInfo.beats}.`,
+			computerSelection
+		);
 		return `You Win this round! ${playerInfo.play} beats ${playerInfo.beats}`;
 	}
 };
 
-let updateResults = () => {
-	if (playerWins < 5 && computerWins < 5) {
-		results.textContent = `You : ${playerWins} VS CPU : ${computerWins}`;
+let updateResults = (summary, enemyWeapon) => {
+	roundNumber.textContent = `Rounds Played: ${round}`;
+	score.textContent = `Your Lives: ${5 - computerWins} ︱ Enemy's Lives: ${
+		5 - playerWins
+	}`;
+	message.textContent = `Combat Area: ${summary}`;
+	enemyWeaponImage.src = `./images/${enemyWeapon}.png`;
+
+	if(summary.includes("You Win")){
+		switch (playerWins) {
+			case 1:
+				message.textContent = `Combat Area: Let's go! Points on the board. ${summary}`;
+	
+				break;
+			case 2:
+				message.textContent = `Combat Area: ${summary} You're pretty good at this.`;
+	
+				break;
+			case 3:
+				message.textContent = `Combat Area: Has mankind found it's saviour?  ${summary}`;
+	
+				break;
+			case 4:
+				message.textContent = `Combat Area: One more and you're a hero! ${summary}`;
+	
+				break;
+			default:
+				message.textContent = `Combat Area: ${summary}`;
+		}
+	
 	}
 
-	if (playerWins == 5) {
-		results.textContent = `YOU WIN! You : ${playerWins} VS CPU : ${computerWins}`;
+
+	if(summary.includes("You Lose")){
+		switch (computerWins) {
+			case 1:
+				message.textContent = `Combat Area: Oh No... ${summary}`;
+	
+				break;
+			case 2:
+				message.textContent = `Combat Area: ${summary} Give it another shot.`;
+	
+				break;
+			case 3:
+				message.textContent = `Combat Area: ${summary} It's ok. You got this.`;
+	
+				break;
+			case 4:
+				message.textContent = `Combat Area: ${summary} Don't let us down!`;
+	
+				break;
+			default:
+				message.textContent = `Combat Area: ${summary}`;
+		}
+	
 	}
+
+
+	if(playerWins == 4 && computerWins==4){
+		message.textContent = `Combat Area: Match Point! It all comes down to this! Good luck. ${summary} `;
+
+	}
+
+	
+
+	
+
+	if (playerWins == 5) {
+		message.textContent = `Combat Area: Skrew those Robots! ${summary}`;	
 	if (computerWins == 5) {
-		results.textContent = `YOU LOSE! You : ${playerWins} VS CPU : ${computerWins}`;
+		message.textContent = `Combat Area: You failed us... ${summary}`;	
 	}
 };
+}
 
 // let game = () => {
 // 	let playerChoice = "";
